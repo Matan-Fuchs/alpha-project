4ddfbf5b49a43f30451e119a1dd69769fed0f0a4
Matan-Fuchs <foxm81342010@gmail.com>
2026-06-05 11:48:06 +0300

    Update ExtractAndPreprocess.py
    It wasn't working so I added the underscores and explanations

--- ExtractAndPreprocess.py
+++ ExtractAndPreprocess.py
@@ -1,10 +1,12 @@
-from moabb.datasets import BNCI2014001
-dataset = BNCI2014001()
-dataset.subject_list = [1]
+from moabb.datasets import BNCI2014_001
 from moabb.paradigms import MotorImagery
-paradigm = MotorImagery(
-    n_classes = 2,
-    fmin = 8.0, fmax = 32.0,
-    tmin = 0.5, tmax = 2.5)
-X, y, metadata = paradigm.get_data(dataset = dataset, subjects = [1])
-print(f"data shape: {X.shape}")
+
+dataset = BNCI2014_001()    # download the data
+dataset.subject_list = [1]  # only for the first subject
+
+paradigm = MotorImagery(    # preprocessing settings
+    n_classes = 2,          # binary filter: to only right hand vs left hand
+    fmin = 8.0, fmax = 32.0, # bandpass: only take signals whose frequency is in the relevant domain
+    tmin = 0.5, tmax = 2.5) # epoch: divide into time windows
+X, y, metadata = paradigm.get_data(dataset = dataset, subjects = [1]) # apply preprocessing
+print(f"data shape: {X.shape}") # print processed data
